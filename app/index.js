1650e0a0762a19a7927305f30f9a7b9cb27c1238
Cesar Augusto <cesardeazevedo@outlook.com>
2017-10-10 02:58:52 -0300

    Render with hydrate

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 /* @flow */
 import React from 'react';
-import { render } from 'react-dom';
+// $FlowFixMe
+import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
@@ -10,9 +11,6 @@ const preloadedState = window.__PRELOADED_STATE__;
 const store = configureStore(preloadedState);
 const rootElement = document.getElementById('root');
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  rootElement
-);
+const { form } = store.getState().app;
+
+hydrate(<Provider store={store}><App form={form} /></Provider>, rootElement);
